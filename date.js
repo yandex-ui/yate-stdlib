99c4546889fe14aa859e76870aa8f80248a3aa07
Alexey Androsov <doochik@ya.ru>
2013-07-18 18:12:59 +0400

    better variables names

--- date.js
+++ date.js
@@ -8,20 +8,20 @@
         return today;
     };
 
-    yrExternals['date-is-today'] = function(num) {
-        var timestamp = parseInt(num, 10) * 1000;
+    yrExternals['date-is-today'] = function(timestamp) {
+        timestamp = parseInt(timestamp, 10) * 1000;
         return createToday().getTime() <= timestamp;
     };
 
-    yrExternals['date-is-week-current'] = function(num) {
-        var timestamp = parseInt(num, 10) * 1000;
+    yrExternals['date-is-week-current'] = function(timestamp) {
+        timestamp = parseInt(timestamp, 10) * 1000;
         var weekAgo = new Date(createToday().getTime() - 7 * 24 * 3600 * 1000);
 
         return weekAgo.getTime() <= timestamp;
     };
 
-    yrExternals['date-is-yesterday'] = function(num) {
-        var timestamp = parseInt(num, 10) * 1000;
+    yrExternals['date-is-yesterday'] = function(timestamp) {
+        timestamp = parseInt(timestamp, 10) * 1000;
         var today = createToday();
         var yesterday = new Date(today.getTime() - 24 * 3600 * 1000);
 
